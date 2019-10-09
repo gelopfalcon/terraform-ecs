806f3ac8b57cabed7fe5fe05043b33021e33b2f8
Gerardo Lopez Falcon <gelopfalcon@gmail.com>
2019-10-09 15:14:13 -0600

    delete

--- terraform/modules/ec2/output.tf
+++
@@ -1,3 +0,0 @@
-output "ecs-service-role-arn" {
-  value = "${aws_iam_role.ecs-service-role.arn}"
-}
--- terraform/modules/lb/output.tf
+++
@@ -1,3 +0,0 @@
-output "alb_target_group-arn" {
-  value = "${aws_alb_target_group.rda-maxar-alb-tg.arn}"
-}
